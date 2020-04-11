feb859ce2ff7aabe243d2a2ea597c1b86b326d16
Alexandr Shshelokov <edgyestlord@gmail.com>
2020-04-11 22:33:58 +0700

    lab 3 almost done

--- lab3/main.c
+++ lab3/main.c
@@ -1,114 +1,196 @@
 #include <stdio.h>
-#include <openmpi/mpi.h>
 #include <stdlib.h>
+#include <openmpi/mpi.h>
 
-#define nrows 2
-#define ncols 2
-#define ndims 2
-#define n1 2
-#define n2 4
-#define n3 4
+#define A_rows 16 ///n1
+#define A_cols 8 ///n2
+#define B_rows 8 /// == n2
+#define B_cols 16 /// == n3
+#define grid_columns 2
+#define grid_rows 2
+#define n_dims 2
+#define x 0
+#define y 1
 
-double *fillMatrix(int row, int col, double value) {
-    double *matrix = (double *) malloc(sizeof(double) * row * col);
-    for (int i = 0; i < row; ++i) {
-        for (int j = 0; j < col; ++j) {
-                if(i!=j)
-                matrix[i * col + j] = value;
-                else matrix[i * col + j] = 4;
-            }
-
-        }
-
-
-
-    return matrix;
+void init_grid_comm(MPI_Comm *grid_comm, int coords[n_dims], int dims[n_dims]) {
+    int periods[n_dims] = {0, 0};
+    MPI_Cart_create(MPI_COMM_WORLD, n_dims, dims, periods, 0, grid_comm);
+    int rank;
+    MPI_Comm_rank(*grid_comm, &rank);
+    MPI_Cart_coords(*grid_comm, rank, n_dims, coords);
 }
 
 
+void init_helper_comms(MPI_Comm grid, MPI_Comm *rows_comm, MPI_Comm *col_comm, int coords[n_dims]) {
+    MPI_Comm_split(grid, coords[y], coords[x], rows_comm);
+    MPI_Comm_split(grid, coords[x], coords[y], col_comm);
+}
+
+
+void distribute_matrices(double *matrixA, double *matrixB, double *partA, double *partB, MPI_Comm row_comm,
+                         MPI_Comm col_comm,const int coords[n_dims]) {
+    if(coords[x] == 0) {
+        MPI_Scatter(matrixA, A_rows * A_cols / grid_rows, MPI_DOUBLE, partA, A_rows * A_cols / grid_rows, MPI_DOUBLE, 0,
+                    col_comm);
+    }
+    MPI_Bcast(partA, A_rows * A_cols / grid_rows, MPI_DOUBLE, 0, row_comm);
+    if(coords[y] == 0){
+
+        MPI_Datatype aux_t;
+        MPI_Datatype b_column_t;
+        MPI_Type_vector(B_rows, B_cols / grid_columns, B_cols, MPI_DOUBLE, &aux_t);
+        MPI_Type_commit(&aux_t);
+        MPI_Type_create_resized(aux_t, 0, B_cols/ grid_columns * sizeof(double), &b_column_t);
+        MPI_Type_commit(&b_column_t);
+
+        MPI_Datatype recv_t;
+        PMPI_Type_contiguous(B_rows * B_cols / grid_columns, MPI_DOUBLE, &recv_t);
+        MPI_Type_commit(&recv_t);
+
+        MPI_Scatter(matrixB, 1, b_column_t,partB, 1, recv_t,0, row_comm);
+
+        MPI_Type_free(&b_column_t);
+        MPI_Type_free(&aux_t);
+        MPI_Type_free(&recv_t);
+    }
+
+    MPI_Bcast(partB,B_rows * B_cols / grid_columns,MPI_DOUBLE,0,col_comm);
+
+
+
+}
+
+void fill_matrix(double *matrix, int row, int col, double value) {
+    for (int i = 0; i < row; ++i) {
+        for (int j = 0; j < col; ++j) {
+            if (i == j) {
+                matrix[i * col + j] = 4;
+            } else {
+                matrix[i * col + j] = value;
+            }
+        }
+    }
+}
+
+
+void matrix_mult(const double *A, const double *B, double *C,int ARows,int AColumns, int BRows,int BColumns){
+    for (int i = 0; i < ARows; i++) {
+        for (int j = 0; j < BColumns; j++) {
+            double sum = 0;
+
+            for (int k = 0; k < AColumns; k++) {
+                sum += A[i * AColumns + k] * B[k * BColumns + j];
+            }
+
+            C[i * BColumns + j] = sum;
+        }
+    }
+}
+
+
+
+
 int main(int argc, char **argv) {
-    int dims[ndims] = {nrows, ncols};
-    int periods[ndims] = {0, 0};
-    int coords[ndims];
-    int reorder = 0;
-    ///double *partA, partB, PartC;
-    int A_rows = n1/nrows;
-    int B_cols = n3/ncols;
-
-
-
-    int rank;
-    double *A, *B, *C;
-    MPI_Comm gridComm;
-    MPI_Comm rowComm, collComm;
-
     MPI_Init(&argc, &argv);
-    MPI_Cart_create(MPI_COMM_WORLD, ndims, dims, periods, reorder, &gridComm);
-    MPI_Comm_rank(gridComm, &rank);
-    MPI_Cart_coords(gridComm, rank, ndims, coords);
-    if (rank == 0) {
-        A = fillMatrix(n1,n2,1.0);
-        B = fillMatrix(n1,n2,2.0);
-       C = fillMatrix(n1,n3,0);
-        /*
-        for (int i = 0; i < n1; ++i) {
-            for (int j = 0; j < n2; ++j) {
-                printf("%lf ", A[i * n2 + j]);
+    int dims[n_dims] = {grid_columns, grid_rows};
+    int coords[n_dims];
+    MPI_Comm grid_comm, row_comm, col_comm;
+    init_grid_comm(&grid_comm, coords, dims);
+    init_helper_comms(grid_comm, &row_comm, &col_comm, coords);
+    int rank;
+    MPI_Comm_rank(grid_comm, &rank);
+    double *A = NULL;
+    double *B = NULL;
 
+    double *C =NULL;
+    double *partC = (double*)malloc(sizeof(double)*A_rows*B_cols/(grid_columns*grid_rows));
+    double *partA = (double *) malloc(sizeof(double) * A_rows * A_cols / grid_rows);
+    double *partB = (double *) malloc(sizeof(double) * B_rows * B_cols / grid_columns);
+
+
+    if (rank == 0) {
+        A = (double *) malloc(sizeof(double) * A_rows * A_cols);
+        B = (double*) malloc(sizeof(double)*B_rows*B_cols);
+        C = (double*)malloc(sizeof(double)*A_rows*B_cols);
+        fill_matrix(A, A_rows, A_cols, 2);
+        fill_matrix(B,B_rows,B_cols,2);
+        printf("matrix A\n");
+        for (int i = 0; i < A_rows; ++i) {
+            for (int j = 0; j < A_cols; ++j) {
+                printf("%lf ", A[i * A_cols + j]);
             }
             printf("\n");
-
-
         }
-         */
+        printf("matrix B\n");
 
 
-    }
-    ///color - coords[0] - row - unique color /// key - coords [1] - ordering
-    MPI_Comm_split(gridComm, coords[0], coords[1], &rowComm);
-    MPI_Comm_split(gridComm, coords[1], coords[0], &collComm);
-    double *partA = (double*)malloc(sizeof(double)*A_rows*n2);
-    double *partB = (double*)malloc(sizeof(double)*n2*B_cols);
-    double *partC = (double*)malloc(sizeof(double)*A_rows*B_cols);
-
-    /// тут надо раскидать по столбцам и строкам матрицы
-    ///
-    if (coords[0] == 0) {
-        MPI_Scatter(B,B_cols*n2,MPI_DOUBLE,partB,B_cols*n2,MPI_DOUBLE,0,rowComm);
-
-
-
-    }
-    MPI_Bcast(partB,n2*B_cols,MPI_DOUBLE,0,collComm);
-
-    if (coords[1] == 0) {
-        MPI_Scatter(A,A_rows*n2,MPI_DOUBLE,partA,A_rows*n2,MPI_DOUBLE,0,collComm);
-        ////send &recv a in the row
+        for (int i = 0; i < B_rows; ++i) {
+            for (int j = 0; j < B_cols; ++j) {
+                printf("%lf ", B[i * B_cols + j]);
+            }
+            printf("\n");
+        }
+        printf("//////////////\n");
 
 
 
 
     }
-    MPI_Bcast(partA,n2*A_rows,MPI_DOUBLE,0,rowComm);
-    if (rank == 0) {
+    distribute_matrices(A,B,partA,partB,row_comm,col_comm,coords);
+    matrix_mult(partA,partB,partC,A_rows/grid_rows,A_cols,B_rows/grid_rows,B_cols/grid_columns);
+    MPI_Gather(partC,A_rows*B_cols/(grid_columns*grid_rows),MPI_DOUBLE,C,A_rows*B_cols/(grid_columns*grid_rows),MPI_DOUBLE,0,grid_comm);
+    if(rank ==0){
+        for(int i =0; i < A_rows;++i) {
+            for (int j = 0; j < B_rows; ++j) {
+                printf("%lf ", C[i * B_rows + j]);
+            }
+            printf("\n");
+        }
+
 
 
     }
-    printf("coordinates is %d %d\n",coords[0],coords[1]);
-    for(int i = 0;i < n3;++i){
-        for(int j = 0; j < B_cols;++j){
-            printf("%lf\n",partB[i*B_cols+j]);
 
+
+/*
+    printf(" coords is %d %d \n", coords[x], coords[y]);
+    for(int i = 0; i < A_rows/grid_rows;++i){
+        for(int j = 0; j < B_cols/grid_columns;++j){
+            printf("%lf ",partC[i*B_cols/grid_columns+j]);
         }
         printf("\n");
     }
+    /*
+    /*
+        for (int i = 0; i < B_rows / grid_rows; ++i) {
+            for (int j = 0; j < A_cols; ++j) {
+                printf("%lf ", partA[i * A_cols + j]);
+            }
+            printf("\n");
+        }
+        */
+    /*
+    for(int i = 0; i < B_rows;++i){
+        for(int j = 0; j < B_cols/grid_columns;++j)
+        printf("%lf ", partB[i*B_cols/grid_columns+j]);
+    }
+    printf("\n");
+     */
+    if(rank ==0){
+        free(A);
+        free(B);
+        free(C);
+    }
+    free(partA);
+    free(partB);
+    free(partC);
 
 
-    MPI_Comm_free(&rowComm);
-    MPI_Comm_free(&collComm);
-    MPI_Comm_free(&gridComm);
+
+
+    MPI_Comm_free(&grid_comm);
+    MPI_Comm_free(&col_comm);
+    MPI_Comm_free(&row_comm);
     MPI_Finalize();
-
-
     return 0;
 }
